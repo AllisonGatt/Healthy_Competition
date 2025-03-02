9b7d4c1e4aee04de313eef2fb909730b14a3f2c3
AllisonGatt <116971163+AllisonGatt@users.noreply.github.com>
2025-03-02 14:05:40 -0500

    more sprint 1

--- src/urls.py
+++ src/urls.py
@@ -3,8 +3,8 @@ from src import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("", views.index, name="home"),
     path('signup/', views.signup, name='signup'),
-    path('login/', auth_views.LoginView.as_view(template_name='src/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
--- src/views.py
+++ src/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 
-def home(request):
+def index(request):
     return render(request, 'index.html')
 
 def signup(request):
